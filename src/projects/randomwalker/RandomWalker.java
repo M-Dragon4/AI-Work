6537b5315ee624cd16f5bc53ef85eda2fe1b950e
M-Dragon4 <max4battles@gmail.com>
2021-08-29 22:45:04 -0400

    Began button implementation

--- src/projects/randomwalker/RandomWalker.java
+++ src/projects/randomwalker/RandomWalker.java
@@ -4,42 +4,67 @@ import util.RandomGenerator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 /**
  * This is a Random Walker project to go along with The Coding Train's Nature of Code video series, but adapted for Java.
- * TODO: Make some fun pathfinding techniques for the Walker
+ * TODO: Make some fun pathfinding techniques for the Walker; add walk/stop buttons to control when the Walker moves
  */
 public class RandomWalker {
 
     private static boolean running = false;
     public static final int ORIGIN_X = 0;
     public static final int ORIGIN_Y = 0;
-    public static final int WIDTH = 640;
-    public static final int HEIGHT = 480;
-    public static final Dimension canvasSize = new Dimension(WIDTH, HEIGHT);
+    public static final int CANVAS_WIDTH = 640;
+    public static final int CANVAS_HEIGHT = 480;
+    public static final int BUTTON_WIDTH = 640;
+    public static final int BUTTON_HEIGHT = 36;
+    public static final Dimension CANVAS_SIZE = new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT);
+    public static final Dimension BUTTON_SIZE = new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT);
     public static Walker prevWalker;
     public static JFrame frame;
+    public static JPanel container;
     public static Canvas canvas;
+    public static JPanel buttonPanel;
+    public static JButton walkButton;
+    public static JButton stopButton;
 
     public static void init() {
         frame = new JFrame("Random Walker");
+        container = new JPanel();
+
+        walkButton = new JButton("Walk");
+        stopButton = new JButton("Stop");
+
+        buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.WHITE);
+        buttonPanel.setPreferredSize(BUTTON_SIZE);
+        buttonPanel.add(walkButton);
+        buttonPanel.add(stopButton);
+        canvas = new Canvas();
+        canvas.setPreferredSize(CANVAS_SIZE);
+
+        container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
+        container.add(buttonPanel);
+        container.add(canvas);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setVisible(true);
-        canvas = new Canvas();
-        canvas.setPreferredSize(canvasSize);
-        frame.add(canvas);
+        frame.add(container);
         frame.pack();
+        frame.setVisible(true);
         prevWalker = new Walker();
         canvas.addWalker(prevWalker);
     }
 
     public static void main(String[] args) throws InterruptedException {
-        running = true;
+        boolean running = true;
         init();
         int i = 0;
+
         while(running) {
             Walker walker = new Walker(prevWalker.getWalkerX(), prevWalker.getWalkerY());
             walker.walk();
@@ -101,12 +126,12 @@ public class RandomWalker {
         }
 
         public boolean isAtBottomEdge() {
-            if (y + WALKER_HEIGHT > HEIGHT - WALKER_HEIGHT) return true;
+            if (y + WALKER_HEIGHT > CANVAS_HEIGHT - WALKER_HEIGHT) return true;
             else return false;
         }
 
         public boolean isAtRightEdge() {
-            if (x + WALKER_WIDTH > WIDTH - WALKER_WIDTH) return true;
+            if (x + WALKER_WIDTH > CANVAS_WIDTH - WALKER_WIDTH) return true;
             else return false;
         }
 
