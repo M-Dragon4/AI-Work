b0f5d0b4a7a7caef06fba997bc3bfc3d755c8151
M-Dragon4 <max4battles@gmail.com>
2022-01-26 21:31:18 -0500

    Fixed HeightMap uniqueness issue Initial tile no longer randomly selected

--- src/projects/neldermead/HeightMap.java
+++ src/projects/neldermead/HeightMap.java
@@ -3,29 +3,15 @@ package projects.neldermead;
 import java.util.Random;
 
 /**
- * TODO: Fix lack of uniqueness across generations (Is a different noise function required? It's not a product of which
- *  tile is chosen as the initial tile...)
+ * TODO: Play around with STRETCH_FACTOR to find the best value for it
  */
 public class HeightMap {
 
-    private final double STRETCH_FACTOR = 28.0;
+    private final double STRETCH_FACTOR = 32.0;
     private Point[] map;
     private double elevationMax, elevationMin;
 
-    private final int[] P = new int[512], PERMUTATION = { 151,160,137,91,90,15,
-            131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-            190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-            88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-            77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-            102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-            135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-            5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-            223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-            129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-            251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-            49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-            138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-    };
+    private final int[] P = new int[512], PERMUTATION = new int[256];
 
     /**
      * Empty Constructor
@@ -55,6 +41,8 @@ public class HeightMap {
      * @param elevationMin the minimum elevation [z] of the map
      */
     public HeightMap(int canvasWidth, int canvasHeight, int tileSize, double elevationMax, double elevationMin) {
+        permute();
+
         this.elevationMax = elevationMax;
         this.elevationMin = elevationMin;
 
@@ -67,36 +55,32 @@ public class HeightMap {
 
         this.map = new Point[numTilesX * numTilesY];
 
-//        for (int t = 0; t < numTilesX * numTilesY; t++) {
-//            double x = t % numTilesX;
-//            double y = (t - x) / numTilesX;
-//            Random r = new Random();
-//            x += r.nextDouble();
-//            y += r.nextDouble();
-//
-//            this.map[t] = new Point(new double[]{x, y, lerp(noise(x, y, STRETCH_FACTOR), (elevationMin + elevationMax) / 2, elevationMax)}, tileSize);
-//        }
-
-        Random r1 = new Random();
-        int i = r1.nextInt(numTilesX * numTilesY);
-        System.out.println(i + " " + numTilesX * numTilesY);
-        for (int t = 0; t < i; t++) {
+        Random r = new Random();
+        for (int t = 0; t < numTilesX * numTilesY; t++) {
             double x = t % numTilesX;
             double y = (t - x) / numTilesX;
-            Random r2 = new Random();
-            x += r2.nextDouble();
-            y += r2.nextDouble();
+            x += r.nextDouble();
+            y += r.nextDouble();
 
             this.map[t] = new Point(new double[]{x, y, lerp(noise(x, y, STRETCH_FACTOR), (elevationMin + elevationMax) / 2, elevationMax)}, tileSize);
         }
-        for (int t = i; t < numTilesX * numTilesY; t++) {
-            double x = t % numTilesX;
-            double y = (t - x) / numTilesX;
-            Random r2 = new Random();
-            x += r2.nextDouble();
-            y += r2.nextDouble();
+    }
 
-            this.map[t] = new Point(new double[]{x, y, lerp(noise(x, y, STRETCH_FACTOR), (elevationMin + elevationMax) / 2, elevationMax)}, tileSize);
+    /**
+     * Fills the PERMUTATION array with the numbers 0 to 255 in a random order without repetition
+     */
+    public void permute() {
+        for (int i = 0; i < PERMUTATION.length; i++) {
+            PERMUTATION[i] = i;
+        }
+
+        Random r = new Random();
+        int rIndex, rIndexVal;
+        for (int i = 0; i < PERMUTATION.length; i++) {
+            rIndex = r.nextInt(PERMUTATION.length);
+            rIndexVal = PERMUTATION[rIndex];
+            PERMUTATION[rIndex] = PERMUTATION[i];
+            PERMUTATION[i] = rIndexVal;
         }
     }
 
